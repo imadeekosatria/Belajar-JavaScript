117e2fd264736cd52de5930c3b34c702a0fc9795
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-02-15 22:55:56 +0800

    Declaring Function

---
+++ Fungsi/Declaring Function.js
@@ -0,0 +1,79 @@
+/*
+Hapus tanda // untuk menjalankan kode!
+*/
+//================ Parameter & Argument =================
+function greeting() {
+    console.log("Good Morning!")
+}
+
+greeting();
+
+/* output
+Good Morning!
+*/
+
+function greeting(name, language) {
+    if(language === "English") {
+        console.log(`Good Morning ${name}!`);
+    } else if (language === "French") {
+        console.log(`Bonjour ${name}!`);
+    } else {
+        console.log(`Selamat Pagi ${name}!`);
+    }
+}
+
+greeting("Harry", "French");
+
+/* output
+Bonjour Harry!
+*/
+
+//================ Fungsi Return =================
+function multiply(a, b) {
+    return a * b;
+}
+
+let result = multiply(10, 2)
+console.log(result)
+
+/* output
+20
+*/
+
+function greeting(name, language) {
+    if(language === "English") {
+        return `Good Morning ${name}!`
+    } else if (language === "French") {
+        return `Bonjour ${name}!`;
+    } else {
+        return `Selamat Pagi ${name}!`;
+    }
+}
+
+let greetingMessage = greeting("Harry", "French");
+console.log(greetingMessage);
+
+/* output
+Bonjour Harry!
+*/
+
+
+//================ Expression Function =================
+/**
+* expression adalah kode atau instruksi yang mengembalikan nilai
+*/
+const greeting = function(name, language) {
+    if(language === "English") {
+        return "Good Morning " + name + "!";
+    } else if (language === "French") {
+        return "Bonjour " + name + "!";
+    } else {
+        return "Selamat Pagi " + name + "!";
+    }
+}
+
+console.log(greeting('Ron', 'English'));
+
+/* output
+Good Morning Ron!
+ */
