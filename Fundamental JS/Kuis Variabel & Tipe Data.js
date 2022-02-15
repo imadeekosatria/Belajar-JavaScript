d7117a60c9d7c9127cd915e214d3bad99b25cb23
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-02-15 22:23:48 +0800

    Kuis Variabel & tipe data, Logika, If else, array, object

---
+++ Fundamental JS/Kuis Variabel & Tipe Data.js
@@ -0,0 +1,21 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+const firstName = 'I Made Eko';
+const lastName = 'Satria Wiguna';
+const age = 22;
+const isMarried = false;
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+module.exports = {
+  firstName, lastName, age, isMarried,
+};
