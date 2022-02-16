ee42ca632663abe4c666dee7384d2813ae19c7a3
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-02-17 00:15:25 +0800

    Closure

---
+++ Fungsi/Closure.js
@@ -0,0 +1,56 @@
+/*
+Hapus tanda // untuk menjalankan kode!
+*/
+
+//================ Closure ================
+function init() {
+    var name = 'Obi Wan';   // Variabel lokal di dalam scope fungsi init
+    
+    function greet() {      // Inner function, merupakan contoh closure
+        console.log(`Halo, ${name}`);   // Memanggil variabel yang dideklarasikan di parent function
+    }
+
+    greet();
+}
+
+init();
+
+/* output
+Halo, Obi Wan
+ */
+
+function init() {
+    var name = 'Obi Wan';
+
+    function greet() {
+        console.log(`Halo, ${name}`);
+    }
+
+    return greet;
+}
+
+let myFunction = init();
+myFunction();
+
+/* output
+Halo, Obi Wan
+ */
+
+let add = () => {
+    let counter = 0; // <= fungsi closure 
+    return () => {
+        return ++counter;
+    };
+}
+
+let addCounter = add();
+
+console.log(addCounter());
+console.log(addCounter());
+console.log(addCounter());
+
+/* output
+1
+2
+3
+ */
