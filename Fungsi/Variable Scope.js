11d35435c52b73cec8c99b18094fa5945450dfe7
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-02-16 22:02:32 +0800

    Variable Scope

---
+++ Fungsi/Variable Scope.js
@@ -0,0 +1,17 @@
+/*
+Hapus tanda // untuk menjalankan kode!
+*/
+//================ Variable Scope =================
+
+// global variable, dapat diakses pada parent() dan child()
+const a = 'a'; 
+ 
+function parent() {
+    // local variable, dapat diakses pada parent() dan child(), tetapi tidak dapat diakses di luar dari fungsi tersebut.
+    const b = 'b'; 
+    
+    function child() {
+        // local variable, dapat diakses hanya pada fungsi child().
+        const c = 'c';
+    }
+}
