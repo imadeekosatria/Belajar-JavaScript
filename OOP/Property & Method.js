ed3753f6af0c5feea8c7200aa2edba7b78c91731
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-02-21 22:40:01 +0800

    constructor

--- OOP/Property & Method.js
+++ OOP/Property & Method.js
@@ -9,6 +9,7 @@ class Mail {
         this.contacts = [];
         this.yourOtherProperty = 'the values';
     }
+    //================ Class Methods =================
     sendMessage(msg, to) {
         console.log(`you send: ${msg} to ${to} from ${this.from}`);
         this.contacts.push(to); // menyimpan kontak baru
@@ -16,22 +17,23 @@ class Mail {
 }
  
 // cara 2
-// function Mail() {
-//     this.from = 'pengirim@dicoding.com';
-//     this.contacts = [];
-//     this.yourOtherPrototype = 'the values';
-// };
+function Mail() {
+    this.from = 'pengirim@dicoding.com';
+    this.contacts = [];
+    this.yourOtherPrototype = 'the values';
+};
  
-// Mail.prototype.sendMessage = function (msg, to) {
-//     console.log(`you send: ${msg} to  ${to} from ${this.from}`);
-//     this.contacts.push(to); // menyimpan kontak baru
-// };
+Mail.prototype.sendMessage = function (msg, to) {
+    console.log(`you send: ${msg} to  ${to} from ${this.from}`);
+    this.contacts.push(to); // menyimpan kontak baru
+};
 
 class Mail {
     constructor() {
         this.from = 'pengirim@dicoding.com';
         this.contacts = [];
     }
+    //================ Class Methods =================
     sendMessage(msg, to, from) {
         console.log(`you send: ${msg} to ${to} from ${from}`);
         // from di sini merujuk ke `from` parameter, bukan ke `from` dari global value yaitu pengirim@dicoding.com
@@ -45,3 +47,40 @@ mail1.sendMessage('hallo', 'penerima@dicoding.com', 'aku@gmail.com');
 /**
 you send: hallo to penerima@dicoding.com from aku@gmail.com
 **/
+
+/**
+ * Static Methods
+ */
+class Mail{
+    static isValidPhone(phone) {
+      return typeof phone === 'number';
+    }
+}
+Mail.isValidPhone(089000000000) //true
+
+/**
+ * Constructor
+ */
+
+// cara 1
+class Mail {
+    constructor(author) {
+        this.from = 'pengirim@dicoding.com';
+        console.log('is instantiated', author);
+    };
+}
+ 
+// cara 2
+function Mail(author) {
+    this.from = author;
+    console.log('is instantiated', author);
+}
+
+const mail1 = new Mail("emailku@dicoding.com");
+
+/**
+ * Karena JavaScript bukan bahasa dengan dukungan static type maka sebenarnya kita dapat 
+ * melakukan instansiasi dengan parameter sesuka kita:
+ */
+const mail1 = new Mail(085000111222); // misalkan untuk SMS
+const mail2 = new Mail("emailku@dicoding.com"); // misalkan untuk Email
